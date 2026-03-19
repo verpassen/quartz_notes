7ba01c28fb9b364ae00db19f1385a8e2d73a42c3
Brent <brentchang0513@gmail.com>
2026-03-19 20:36:01 +0800

    reconfigure the quartz to ignore the draft folder

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "zh-TW",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian","draft/"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
